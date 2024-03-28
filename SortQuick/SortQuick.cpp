839c0c509d57073fae6f7973b24c06033b53f33d
vararais <varadigmaisnann@gmail.com>
2024-03-28 20:36:44 +0700

    Menambah For

--- SortQuick/SortQuick.cpp
+++ SortQuick/SortQuick.cpp
@@ -23,3 +23,10 @@ void input() {
         cout << "\n-------------------" << endl;
         cout << "\nEnter Array Element" << endl;
         cout << "\n-------------------" << endl;
+
+            for (int i = 0; i < n; i++)
+            {
+                cout << "<" << (i + 1) << ">";
+                cin >> arr[i];
+            }
+        }
