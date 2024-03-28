8cfe10f2b35fc9e90b0dbc36a6224bf8bbb13f35
vararais <varadigmaisnann@gmail.com>
2024-03-28 20:45:33 +0700

    Menambah Void Display

--- SortQuick/SortQuick.cpp
+++ SortQuick/SortQuick.cpp
@@ -94,6 +94,21 @@ void q_short(int low, int high)
         }
 
 
+void display() {
+    cout << "\n-------------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "---------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
+
+
         
     
 
