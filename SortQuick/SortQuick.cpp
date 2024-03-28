d50d7971041e40f9683f16da33513dd02fe82dec
vararais <varadigmaisnann@gmail.com>
2024-03-28 20:34:15 +0700

    Menambah Void Input

--- SortQuick/SortQuick.cpp
+++ SortQuick/SortQuick.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+
 int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+
+void input() {
+    while (true)
+    {
+        cout << "Masukan Panjang Element Array: ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum Panjang Array Adalah 20" << endl;
+    }
 
